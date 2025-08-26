db6a55cdff9f7f68408d296e9de280e1d6e9a40b
Seungmin7979 <safetmdals79@gmail.com>
2025-08-26 20:54:22 +0900

    [Feat] UIManager 싱글톤 사용

--- Assets/02. Scripts/UIManager.cs
+++ Assets/02. Scripts/UIManager.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager Instance;
+
     [SerializeField] private UIMainMenu uiMainMenu;
     [SerializeField] private UIStatus uiStatus;
     [SerializeField] private UIInventory uiInventory;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 }
